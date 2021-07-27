71eba305ab274771a3db195360ac6ef827697340
Duongabbcd <64185740+Duongabbcd@users.noreply.github.com>
2021-07-27 11:23:32 +0700

    Update TextDetectionActivity.java

--- app/src/main/java/com/example/mldemo/TextDetectionActivity.java
+++ app/src/main/java/com/example/mldemo/TextDetectionActivity.java
@@ -1,111 +1,266 @@
 package com.example.mldemo;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.BitmapCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.util.SparseArray;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.List;
 
 public class TextDetectionActivity extends AppCompatActivity {
-    private Button captureImageBtn ,detectTextBtn ;
-    private ImageView imageView;
-    private TextView textView ;
+    private static final int CAMERA_REQUEST_CODE =200 ;
+    private static final int STORAGE_REQUEST_CODE =400 ;
+    private static final int IMAGE_PICK_GALLERY_CODE =1000 ;
+    private static final int IMAGE_PICK_CAMERA_CODE =1001;
 
-    static final int REQUEST_IMAGE_CAPTURE = 1 ;
-    private Bitmap imageBitmap;
+    String cameraPermission[];
+    String storagePermission[];
+
+    EditText mResultEt ;
+    ImageView mPreviewIv ;
+    Uri image_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_detection);
 
-        captureImageBtn =findViewById(R.id.capture_image_btn) ;
-        detectTextBtn= findViewById(R.id.detect_text_image_btn) ;
-        imageView = findViewById(R.id.imageView) ;
-        textView =findViewById(R.id.text_display) ;
-
-        captureImageBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dispatchTakePictureIntent();
-                textView.setText("");
-            }
-        });
-        detectTextBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                detectTextBtnFromImage();
-            }
-        });
-    }
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } catch (ActivityNotFoundException e) {
-            // display error state to the user
-        }
+        ActionBar actionBar =getSupportActionBar() ;
+        actionBar.setSubtitle("Click + buton to insert image"); ;
+        mResultEt=findViewById(R.id.resultEt);
+        mPreviewIv=findViewById(R.id.imageView) ;
+        //camera permission
+        cameraPermission = new String[]{Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        //storage permission
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true ;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id  = item.getItemId();
+        if(id == R.id.addImage) {
+            showImageImportDialog();
+        }
+
+        if(id ==R.id.settings){
+            Toast.makeText(this,"Setting",Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showImageImportDialog() {
+        //item to display in dialog
+        String[] items = {"Camera", "Gallery"};
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this) ;
+        //set Title
+        dialog.setTitle("Select Image") ;
+        dialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == 0) {
+                    //camera option clicked
+                    if (!checkCameraPermission()) {
+                        //camera permission not allowed it ,request it
+                        requestCameraPermission();
+                    } else {
+                        //permission allowed ,take picture
+                        pickCamera();
+                    }
+                }
+                if (which == 1) {
+                    if(!checkStoragePermission()){
+                        //Storage permission not allowed ,request it
+                        requestStoragePermission();
+                    }
+                    else{
+                        pickGallery();
+                    }
+                }
+            }
+        }) ;
+        dialog.create().show(); //show dialog
+        }
+
+    private void pickGallery() {
+        //intent to pick image form gallery
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        //set intent typre to image
+        intent.setType("image/*");
+        startActivityForResult(intent,IMAGE_PICK_CAMERA_CODE);
+    }
+
+    private void pickCamera() {
+        //intent to take image form camera ,it will also be save to storage to get high quality image
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,"NewPic"); //title of the picture
+        values.put(MediaStore.Images.Media.DESCRIPTION,"Image To Text"); //description
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+        startActivityForResult(cameraIntent,IMAGE_PICK_CAMERA_CODE);
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(this,storagePermission,STORAGE_REQUEST_CODE);
+    }
+
+    private boolean checkStoragePermission() {
+        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                ==(PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this,cameraPermission,CAMERA_REQUEST_CODE);
+    }
+
+    private boolean checkCameraPermission() {
+        /* Check camera permission and return the result
+        * To get high quality image we have to save image to external storage first
+        * before inserting to image view that's why storage permission will also be required */
+        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                ==(PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                ==(PackageManager.PERMISSION_GRANTED);
+
+        return result && result1  ;
+    }
+
+
+    //handle permission result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case CAMERA_REQUEST_CODE:
+            if(grantResults.length > 0){
+                boolean cameraAccepted = grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED ;
+                boolean writeStorageAccepted = grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED ;
+                if(cameraAccepted && writeStorageAccepted){
+                    pickCamera();
+                }else{
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_SHORT).show();
+                }
+            } break;
+
+            case STORAGE_REQUEST_CODE:
+                if(grantResults.length > 0){
+                    boolean writeStorageAccepted = grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED ;
+                    if(writeStorageAccepted){
+                        pickGallery();
+                    }else{
+                        Toast.makeText(this, "permission denied", Toast.LENGTH_SHORT).show();
+                    }
+                } break;
+        }
+    }
+
+    //handle image result
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-        }
-    }
-
-
-    private void detectTextBtnFromImage() {
-        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
-        FirebaseVisionTextRecognizer firebaseVisionTextRecognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
-        firebaseVisionTextRecognizer.processImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
-            @Override
-            public void onSuccess(@NonNull FirebaseVisionText firebaseVisionText) {
-                displayTextFromImage(firebaseVisionText);
+        //got image from camera
+        if(resultCode == RESULT_OK){
+            if(requestCode == IMAGE_PICK_GALLERY_CODE){
+                //got image from gallery now crop it
+                CropImage.activity(data.getData()).setGuidelines(CropImageView.Guidelines.ON)
+                        .start(this);
             }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(TextDetectionActivity.this, "Error !", Toast.LENGTH_SHORT).show();
-                Log.d("Error : ",e.getMessage());
-            }
-        });
-    }
-
-    private void displayTextFromImage(FirebaseVisionText firebaseVisionText) {
-        List<FirebaseVisionText.TextBlock> blockList = firebaseVisionText.getTextBlocks();
-        if(blockList.size() == 0 ){
-            Toast.makeText(this, "No text Found in this Image !", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            for(FirebaseVisionText.TextBlock block : firebaseVisionText.getTextBlocks()){
-                String text = block.getText() ;
-                textView.setText(text);
+            if(requestCode==IMAGE_PICK_CAMERA_CODE){
+                //got image from camera now crop it
+                CropImage.activity(image_uri).setGuidelines(CropImageView.Guidelines.ON)
+                        .start(this);
             }
         }
-    }
+        //get cropped image
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if(resultCode == RESULT_OK){
+                Uri resultUri = result.getUri(); //get image uri
+                mPreviewIv.setImageURI(resultUri);
 
+                //get drawable bitmap for text recognition
+                BitmapDrawable bitmapDrawable = (BitmapDrawable)mPreviewIv.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+
+                TextRecognizer recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
+                if(!recognizer.isOperational()){
+                    Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build() ;
+                    SparseArray<TextBlock> items = recognizer.detect(frame);
+                    StringBuilder sb = new StringBuilder();
+                    //get text from sb until there is no text
+
+                    for(int i = 0 ; i < items.size() ; i++){
+                        TextBlock myItem =items.valueAt(i);
+                        sb.append(myItem.getValue());
+                        sb.append("\n") ;
+                    }
+
+                    //set Text to edit text
+                    mResultEt.setText(sb.toString());
+                }
+            }
+            else if(resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE){
+                Exception error = result.getError();
+                Toast.makeText(this, ""+error, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
